f7aa0ffcab398df8647a8ee5521154c41f47c94d
Bemnet14 <tewoldebemnet@gmail.com>
2024-11-13 16:59:34 +0100

    Changes

--- pageObject/ContentPage.js
+++ pageObject/ContentPage.js
@@ -13,7 +13,8 @@ class ContentPage {
       '//*[@id="content"]/div/p[3]'
     ];
 
-    // Verwachte teksten voor validatie
+   
+    // Expected texts for validation
     this.expectedTexts = [
       'Lorem ipsum egestas posuere vivamus neque facilisis augue cursus litora rhoncus aenean aptent eu quis, odio scelerisque curabitur rhoncus sociosqu velit curae ipsum duis porttitor rhoncus amet. consectetur nostra massa molestie sed imperdiet nulla mauris in cras mauris lobortis feugiat, quis sem sagittis tortor diam vehicula habitant primis ultricies platea et. amet aliquet nisi proin volutpat sapien eget, tincidunt nisl neque habitant tellus, mi commodo congue habitasse est. etiam imperdiet quisque sociosqu vivamus ut libero nibh fames, nullam eleifend adipiscing iaculis faucibus nulla dolor varius, curae sollicitudin habitant aliquet nam quis neque.',
 
@@ -23,14 +24,14 @@ class ContentPage {
     ];
   }
 
-  // Methode om zichtbaarheid van de home, products en contact elementen te valideren
+  /// Method to validate visibility of home, products, and contact elements
   async validateMenuItems() {
     await expect(this.page.locator(this.homeSelector)).toBeVisible();
     await expect(this.page.locator(this.productsSelector)).toBeVisible();
     await expect(this.page.locator(this.contactSelector)).toBeVisible();
   }
 
-  // Methode om de teksten onder de #content-locator te valideren
+  // Method to validate the text under the #content locator
   async validateContentTexts() {
     for (let i = 0; i < this.textSelectors.length; i++) {
       await expect(this.page.locator(this.textSelectors[i])).toContainText(this.expectedTexts[i]);
--- pageObject/LoginPage.js
+++ pageObject/LoginPage.js
@@ -6,39 +6,34 @@ class LoginPage {
     this.loginButton = 'body > main:nth-child(2) > section:nth-child(1) > form:nth-child(2) > fieldset:nth-child(1) > input:nth-child(6)';
     this.userIcon = "(//i[@class='fas fa-user-circle'])[1]";
     this.logoutButton = "(//span[normalize-space()='Sign Out'])[1]";
-    this.url = 'http://127.0.0.1:5500/index.html'; // URL naar de loginpagina
-    this.errorMessageLocator = '.error-message';  // Locatie van de foutmelding (kan aangepast worden naar de juiste selector)
+    this.url = 'http://127.0.0.1:5500/index.html'; 
+  
   }
 
-  // Ga naar de loginpagina
+  // Navigate to the login page
   async goToLoginPage() {
     await this.page.goto(this.url);
   }
 
-  // Vul inloggegevens in en klik op de login knop
+  // Fill in login credentials and click the login button
   async login(email, password) {
     await this.page.fill(this.emailInput, email);
     await this.page.fill(this.passwordInput, password);
     await this.page.locator(this.loginButton).click();
   }
 
-  // Verkrijg de gebruiker die ingelogd is vanuit localStorage
+  /// Get the logged-in user from localStorage
   async getLoggedUser() {
     return await this.page.evaluate(() => localStorage.getItem('logged'));
   }
 
-  // Klik op het gebruikersicoon om uit te loggen
+  // Click on the user icon to log out
   async logout() {
     await this.page.locator(this.userIcon).click();
     await this.page.locator(this.logoutButton).click();
   }
 
-  // Verkrijg de foutmelding na een mislukte login
-  async getErrorMessage() {
-    return await this.page.locator(this.errorMessageLocator);
-  }
-
-  // Zorg ervoor dat de login-knop zichtbaar is
+  // Ensure the login button is visible
   async getLoginButton() {
     return this.page.locator(this.loginButton);
   }
